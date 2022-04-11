de0293333d46daa3c580cb0fd010383650b19129
Flavio Calvo <flavio.calvo@astro.su.se>
2022-04-11 13:02:53 +0200

    Updated the `README.md' file as well as `setup.py' and `setup.cfg'

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # COCOPLOT
-COlor COllapsed PLOTting quick-look and context image software is designed to convey spectral profile information from all of the spatial pixels in a 3D datacube as a single image, using color.
+COlor COllapsed PLOTting software (COCOPLOT) generates quick-look and context images. The aim is to convey spectral profile information from all of the spatial pixels in a 3D datacube via a single 2D image, using color. Filters for red, green, and blue channels are convolved with the datacube to produce an RGB a color image. This process avoids the user needing to scan through many different wavelengths when searching for regions in the datacube that satisfy multiple criteria.
+
+Although applicable to any 3D datacube, this software was inspired by a single thought: what would the Sun look like if we could only see light from one spectral line? In an absorption line, with low emission in the central wavelengths, and high wings on either side, the blue and red cone receptors of our thought experiment would be triggered, making the Sun appear purple. For a strong, narrow emission line the converse is true, and so the line would appear green. A red or blue Doppler-shifted emission would appear in those colours respectively.
 
 ## Citing this method
 These routines were developed as part of a publication by
--- setup.py
+++ setup.py
@@ -3,11 +3,21 @@
 
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
+<<<<<<< Updated upstream
    name             = 'COCOPLOT',
    version          = '0.1.0',
+=======
+   name             = 'COCOPLOTS',
+   version          = '1.0.8',
+>>>>>>> Stashed changes
    description      = 'COlor COllapsed PLOTting quick-look and context image software',
-   author           = 'A. Pietrow',
+   long_description=long_description,
+   long_description_content_type="text/markdown",
+   author           = 'A.G.M. Pietrow, G.J.M. Vissers, C. Robustini, M.K. Druett',
    author_email     = 'alex.pietrow@astro.su.se',
    classifiers      = [
        # How mature is this project? Common values are
@@ -17,15 +27,16 @@ setup(
        "Development Status :: 3 - Alpha",
        "Intended Audience :: Developers",
        "Topic :: Software Development :: Build Tools",
-       "License :: OSI Approved :: BSD 3-Clause License",
+       "License :: OSI Approved :: BSD License",
        "Programming Language :: Python :: 3",
        "Programming Language :: Python :: 3.7",
        "Programming Language :: Python :: 3.8",
        "Programming Language :: Python :: 3.9",
        "Programming Language :: Python :: 3.10",
        "Programming Language :: Python :: 3 :: Only",
+       "Operating System :: OS Independent",
    ],
-   url              = 'https://github.com/mdruett/COCOPLOT',
+   url              = 'https://github.com/mdruett/COCOPLOT/tree/master/Python',
    package_dir      = {'': 'Python'},
    packages         = find_packages(where='Python', include=['cocopy']),
    python_requires  = '>=3.7, <4',
